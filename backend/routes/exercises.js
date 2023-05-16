a24b47a2e43b08d083981ddb0f7203cbc3c2d290
kaushik.bora <herock76@gmail.com>
2023-05-17 02:24:28 +0530

    -fixed bug in description property

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -9,7 +9,7 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req, res) => {
     const username = req.body.username;
-    const description = req.body.username;
+    const description = req.body.description;
     const duration = Number(req.body.duration);
     const date = Date.parse(req.body.date);
 
